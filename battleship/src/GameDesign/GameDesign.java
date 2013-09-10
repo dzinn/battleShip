117783a170159120d69410cfdf49fe22e955b319
TejeshP <Firefly@Albatross.local>
2013-09-10 17:58:58 -0400

    Revert 44b2d5d..67ac26b
    This rolls back to commit 44b2d5d1dea27c89e114dc1680c7756f2b16aacf.

--- battleship/src/GameDesign/GameDesign.java
+++ battleship/src/GameDesign/GameDesign.java
@@ -10,6 +10,11 @@ package GameDesign;
 public class GameDesign {
 	public static void main(String args[]) {
 		UserInterface UI = new UserInterface();
+		DefaultOpponent AI = new DefaultOpponent();
+		
+		AI.setShips();
+
 		UI.start();
+		
 	}
 }
